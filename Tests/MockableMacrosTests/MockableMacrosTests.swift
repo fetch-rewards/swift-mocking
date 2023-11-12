3b3186887ebb0ad82e9044d202cc7ef1c092f470
Cole Campbell <12163026+colemcampbell@users.noreply.github.com>
2023-11-11 18:28:09 -0800

    Remove unused code

--- Tests/MockableMacrosTests/MockableMacrosTests.swift
+++ Tests/MockableMacrosTests/MockableMacrosTests.swift
@@ -1,7 +1,1 @@
-import SwiftSyntaxMacros
-import SwiftSyntaxMacrosTestSupport
-import XCTest
 
-// Macro implementations build for the host, so the corresponding module is not
-// available when cross-compiling. Cross-compiled tests may still make use of
-// the macro itself in end-to-end tests.
