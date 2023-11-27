b31aeedd613576e70850659e5b3ff6764b0b6d3c
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2023-11-26 23:58:51 -0600

    Fix multi-line string indentation

--- Tests/MockableMacrosTests/Mockable_AssociatedTypeTests.swift
+++ Tests/MockableMacrosTests/Mockable_AssociatedTypeTests.swift
@@ -17,12 +17,13 @@ final class Mockable_AssociatedTypeTests: XCTestCase {
 
     func testProtocolAssociatedTypeInheritanceWithOneInheritedType() {
         testMockable { interface, mock in
-            assertMockable("""
-                    \(interface.accessLevel) protocol Dependency {
-                        associatedtype A: Hashable
-                        associatedtype B: Identifiable
-                    }
-                    """,
+            assertMockable(
+                """
+                \(interface.accessLevel) protocol Dependency {
+                    associatedtype A: Hashable
+                    associatedtype B: Identifiable
+                }
+                """,
                 generates: """
                     \(mock.modifiers)class DependencyMock<A: Hashable, B: Identifiable>: Dependency {
                     \(mock.defaultInit)
@@ -34,12 +35,13 @@ final class Mockable_AssociatedTypeTests: XCTestCase {
 
     func testProtocolAssociatedTypeInheritanceWithMultipleInheritedTypes() {
         testMockable { interface, mock in
-            assertMockable("""
-                    \(interface.accessLevel) protocol Dependency {
-                        associatedtype A: Hashable, Identifiable
-                        associatedtype B: Comparable, Equatable, RawRepresentable
-                    }
-                    """,
+            assertMockable(
+                """
+                \(interface.accessLevel) protocol Dependency {
+                    associatedtype A: Hashable, Identifiable
+                    associatedtype B: Comparable, Equatable, RawRepresentable
+                }
+                """,
                 generates: """
                     \(mock.modifiers)class DependencyMock<A: Hashable & Identifiable, B: Comparable & Equatable & RawRepresentable>: Dependency {
                     \(mock.defaultInit)
@@ -53,13 +55,14 @@ final class Mockable_AssociatedTypeTests: XCTestCase {
 
     func testProtocolAssociatedTypeGenericWhereClauses() {
         testMockable { interface, mock in
-            assertMockable("""
-                    \(interface.accessLevel) protocol Dependency<A> where A: Hashable {
-                        associatedtype A: Comparable
-                        associatedtype B: BidirectionalCollection where B.Element: Equatable, B.Element: Identifiable
-                        associatedtype C: RandomAccessCollection where C.Element == String
-                    }
-                    """,
+            assertMockable(
+                """
+                \(interface.accessLevel) protocol Dependency<A> where A: Hashable {
+                    associatedtype A: Comparable
+                    associatedtype B: BidirectionalCollection where B.Element: Equatable, B.Element: Identifiable
+                    associatedtype C: RandomAccessCollection where C.Element == String
+                }
+                """,
                 generates: """
                     \(mock.modifiers)class DependencyMock<A: Comparable, B: BidirectionalCollection, C: RandomAccessCollection>: Dependency where A: Hashable, B.Element: Equatable, B.Element: Identifiable, C.Element == String {
                     \(mock.defaultInit)
