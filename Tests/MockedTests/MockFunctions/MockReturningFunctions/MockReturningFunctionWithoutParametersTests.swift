2192b16c1fdf2e741bf707040deaa47f75e289cf
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2023-12-07 23:31:04 -0600

    Add tests for mock returning functions

---
+++ Tests/MockedTests/MockFunctions/MockReturningFunctions/MockReturningFunctionWithoutParametersTests.swift
@@ -0,0 +1,97 @@
+//
+//  MockReturningFunctionWithoutParametersTests.swift
+//  MockedTests
+//
+//  Created by Gray Campbell on 12/7/23.
+//
+
+import XCTest
+@testable import Mocked
+
+final class MockReturningFunctionWithoutParametersTests: XCTestCase {
+
+    // MARK: Typealiases
+
+    typealias SUT = MockReturningFunctionWithoutParameters<ReturnValue>
+    typealias ReturnValue = Int
+
+    // MARK: Implementation Tests
+
+    func testImplementationDefaultValue() {
+        self.test { sut, _ in
+            guard case .unimplemented = sut.implementation else {
+                XCTFail("Expected implementation to equal .unimplemented")
+                return
+            }
+        }
+    }
+
+    // MARK: Call Count Tests
+
+    func testCallCount() {
+        self.test { sut, invoke in
+            XCTAssertEqual(sut.callCount, .zero)
+
+            sut.implementation = .returns(5)
+
+            _ = invoke()
+            XCTAssertEqual(sut.callCount, 1)
+        }
+    }
+
+    // MARK: Return Values Tests
+
+    func testReturnValues() {
+        self.test { sut, invoke in
+            XCTAssertEqual(sut.returnValues, [])
+
+            sut.implementation = .returns(5)
+
+            _ = invoke()
+            XCTAssertEqual(sut.returnValues, [5])
+
+            sut.implementation = .returns(10)
+
+            _ = invoke()
+            XCTAssertEqual(sut.returnValues, [5, 10])
+        }
+    }
+
+    // MARK: Latest Return Value Tests
+
+    func testLatestReturnValue() {
+        self.test { sut, invoke in
+            XCTAssertNil(sut.latestReturnValue)
+
+            sut.implementation = .returns(5)
+
+            _ = invoke()
+            XCTAssertEqual(sut.latestReturnValue, 5)
+
+            sut.implementation = .returns(10)
+
+            _ = invoke()
+            XCTAssertEqual(sut.latestReturnValue, 10)
+        }
+    }
+}
+
+// MARK: - Helpers
+
+extension MockReturningFunctionWithoutParametersTests {
+    private func test(
+        test: (
+            _ sut: SUT,
+            _ invoke: () -> ReturnValue
+        ) -> Void
+    ) {
+        let (sut, invoke) = SUT.makeFunction(
+            exposedFunctionDescription: MockImplementationDescription(
+                type: Self.self,
+                member: "sut"
+            )
+        )
+
+        test(sut, invoke)
+    }
+}
