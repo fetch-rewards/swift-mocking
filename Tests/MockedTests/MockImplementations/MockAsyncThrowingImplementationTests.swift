02b38718c0e5c295eef4b9c12ce846fccee1ad1d
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2023-12-08 18:07:51 -0600

    Add tests for mock implementations

---
+++ Tests/MockedTests/MockImplementations/MockAsyncThrowingImplementationTests.swift
@@ -0,0 +1,51 @@
+//
+//  MockAsyncThrowingImplementationTests.swift
+//  MockedTests
+//
+//  Created by Gray Campbell on 12/8/23.
+//
+
+import XCTest
+@testable import Mocked
+
+final class MockAsyncThrowingImplementationTests: XCTestCase {
+
+    // MARK: Typealiases
+
+    typealias SUT = MockAsyncThrowingImplementation<ReturnValue>
+    typealias ReturnValue = Int
+
+    // MARK: Call As Function Tests
+
+    func testReturnsCallAsFunction() async throws {
+        let sut: SUT = .returns { 5 }
+
+        let returnValue = try await sut(description: self.description())
+
+        XCTAssertEqual(returnValue, 5)
+    }
+
+    func testThrowsCallAsFunction() async throws {
+        let sut: SUT = .throws { URLError(.badURL) }
+
+        do {
+            _ = try await sut(description: self.description())
+            XCTFail("Expected sut to throw an error")
+        } catch let error as URLError {
+            XCTAssertEqual(error.code, .badURL)
+        } catch {
+            XCTFail("Expected \(error) to equal URLError(.badURL)")
+        }
+    }
+}
+
+// MARK: - Helpers
+
+extension MockAsyncThrowingImplementationTests {
+    private func description() -> MockImplementationDescription {
+        MockImplementationDescription(
+            type: Self.self,
+            member: "sut"
+        )
+    }
+}
