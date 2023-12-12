d991c9f3806f34babb474bdd940bfb37d21fc924
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2023-12-11 20:17:02 -0600

    Add documentation (#15)

--- Sources/Mockable/MockImplementations/MockImplementation.swift
+++ Sources/Mockable/MockImplementations/MockImplementation.swift
@@ -8,15 +8,26 @@
 import Foundation
 import XCTestDynamicOverlay
 
+/// A mock's implementation.
 public enum MockImplementation<Value> {
 
     // MARK: Cases
 
+    /// Triggers a test failure when invoked.
     case unimplemented
+
+    /// Returns a value when invoked.
     case returns(Value)
 
     // MARK: Call As Function
 
+    /// Invokes the implementation, triggering a test failure if the
+    /// implementation is ``MockImplementation/unimplemented`` or returning a
+    /// value if the implementation is ``MockImplementation/returns(_:)``.
+    ///
+    /// - Parameter description: The implementation's description.
+    /// - Returns: A value if the implementation is
+    ///   ``MockImplementation/returns(_:)``.
     func callAsFunction(description: MockImplementationDescription) -> Value {
         switch self {
         case .unimplemented:
