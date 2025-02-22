3495a1fc02fa76c80fa624229b17d0c76db6aa9d
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2025-02-22 13:53:06 -0600

    Remove unused files

--- Sources/Mocked/Models/MockMethods/Protocols/MockThrowingImplementation.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MockThrowingImplementation.swift
-//  Mocked
-//
-//  Created by Gray Campbell on 2/22/25.
-//
-
-/// A throwing implementation for a mock.
-public protocol MockThrowingImplementation {}
