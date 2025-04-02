4efada764918440e3f2b0902b7cc69ee2f57f8ec
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2025-04-02 12:38:08 -0500

    Remove mock's default init when protocol already requires an empty init (#77)

---
+++ Sources/MockedClient/Initializers.swift
@@ -0,0 +1,22 @@
+//
+//  Initializers.swift
+//  MockedClient
+//
+//  Created by Gray Campbell on 3/29/25.
+//
+
+import Foundation
+public import Mocked
+
+/// A protocol for verifying Mocked's handling of initializers.
+///
+/// - Important: Please only use this protocol for permanent verification of
+///   Mocked's handling of initializers. For temporary testing of Mocked's
+///   expansion, use the `Playground` protocol in `main.swift`.
+@Mocked
+public protocol Initializers {
+    init()
+    init(parameter: Int)
+    init(parameters: Int...)
+    init(parameter1: Int, parameter2: Int)
+}
---
+++ Sources/MockedClient/VariadicParameters.swift
@@ -0,0 +1,19 @@
+//
+//  VariadicParameters.swift
+//  MockedClient
+//
+//  Created by Gray Campbell on 3/30/25.
+//
+
+import Foundation
+public import Mocked
+
+/// A protocol for verifying Mocked's handling of variadic parameters.
+///
+/// - Important: Please only use this protocol for permanent verification of
+///   Mocked's handling of variadic parameters. For temporary testing of
+///   Mocked's expansion, use the `Playground` protocol in `main.swift`.
+@Mocked
+public protocol VariadicParameters {
+    func method(strings: String..., integers: Int...)
+}
