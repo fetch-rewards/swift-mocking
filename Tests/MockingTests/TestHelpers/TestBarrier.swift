d2458a3e25897a816c512387e2577c20c476e508
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2025-07-28 10:19:47 -0500

    Run swiftformat

--- Tests/MockingTests/TestHelpers/TestBarrier.swift
+++ Tests/MockingTests/TestHelpers/TestBarrier.swift
@@ -31,7 +31,7 @@ actor TestBarrier {
     // MARK: Properties
 
     /// The default number of tasks used when no task count is specified.
-    static let defaultTaskCount = 1000
+    static let defaultTaskCount = 1_000
 
     /// The continuations waiting to be resumed once all tasks arrive.
     private var continuations: [CheckedContinuation<Void, Never>] = []
