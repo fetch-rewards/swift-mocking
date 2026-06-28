3b2ccf70a4dd156e771fd085fc425f55e49ba5fa
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2026-06-27 20:50:43 -0500

    Add MockReadOnlySubscript and tests
    Adds MockReadOnlySubscript, which wraps MockSubscriptGetter,
    with unit tests.

---
+++ Tests/MockingTests/Models/MockSubscripts/MockReadOnlySubscriptTests.swift
@@ -0,0 +1,74 @@
+//
+//  MockReadOnlySubscriptTests.swift
+//
+//  Copyright © 2026 Fetch.
+//
+
+import Testing
+@testable import Mocking
+
+struct MockReadOnlySubscriptTests {
+
+    // MARK: Typealiases
+
+    typealias SUT = MockReadOnlySubscript<Arguments, Value>
+    typealias Arguments = String
+    typealias Value = Int
+
+    // MARK: Getter Tests
+
+    @Test
+    func getter() async {
+        let (sut, get, _) = self.sut()
+
+        await confirmation(expectedCount: 1) { confirmation in
+            sut.getter.implementation = .uncheckedInvokes { _ in
+                confirmation.confirm()
+                return 5
+            }
+
+            _ = get("a")
+        }
+    }
+
+    // MARK: Reset Tests
+
+    @Test
+    func reset() async throws {
+        let (sut, get, reset) = self.sut()
+
+        sut.getter.implementation = .uncheckedInvokes { _ in 5 }
+
+        try await TestBarrier.executeConcurrently {
+            _ = get("a")
+        }
+        #expect(sut.getter.callCount == TestBarrier.defaultTaskCount)
+
+        try await TestBarrier.executeConcurrently {
+            reset()
+        }
+        #expect(sut.getter.callCount == .zero)
+
+        guard case .unimplemented = sut.getter.implementation else {
+            Issue.record("Expected getter implementation to equal .unimplemented")
+            return
+        }
+    }
+}
+
+// MARK: - Helpers
+
+extension MockReadOnlySubscriptTests {
+    private func sut() -> (
+        subscript: SUT,
+        get: @Sendable (Arguments) -> Value,
+        reset: @Sendable () -> Void
+    ) {
+        SUT.makeSubscript(
+            exposedSubscriptDescription: MockImplementationDescription(
+                type: Self.self,
+                member: "sut"
+            )
+        )
+    }
+}
